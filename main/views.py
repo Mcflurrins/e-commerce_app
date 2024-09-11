842e12c316396df6e12ee6d53e218ca19ff35135
Mcflurrins <flori.andrea.60@gmail.com>
2024-09-11 10:00:37 +0700

    added link to pws

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'app_name' : 'Top Up Diamond Mobil Legen Murah',
+        'app_name' : 'Upcycle Shop',
         'name': 'Flori Andrea Ng',
         'class': 'KKI'
     }
