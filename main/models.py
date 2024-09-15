310379ff775501e03c3620529599c95f6706abe3
Mcflurrins <flori.andrea.60@gmail.com>
2024-09-15 23:27:21 +0700

    push pws

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+import uuid  # add this line at the very top
+
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # add this line
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
