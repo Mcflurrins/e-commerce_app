53d8f02700d5c00bb168a2018892d914b3acef61
Mcflurrins <flori.andrea.60@gmail.com>
2024-11-18 19:04:03 +0700

    More comments to help demos

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -134,20 +134,3 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import logging
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-}
